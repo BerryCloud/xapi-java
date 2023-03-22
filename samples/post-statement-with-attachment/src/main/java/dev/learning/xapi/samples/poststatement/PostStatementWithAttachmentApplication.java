3e768e57face22c44ff54fb0cca5ff32dfea1bcc
Thomas Turrell-Croft <thomasturrell@users.noreply.github.com>
2023-03-22 11:38:48 +0000

    Apply suggestions from code review

--- samples/post-statement-with-attachment/src/main/java/dev/learning/xapi/samples/poststatement/PostStatementWithAttachmentApplication.java
+++ samples/post-statement-with-attachment/src/main/java/dev/learning/xapi/samples/poststatement/PostStatementWithAttachmentApplication.java
@@ -57,13 +57,13 @@ public class PostStatementWithAttachmentApplication implements CommandLineRunner
                         // Add simple text attachment
                         .addAttachment(a -> a.content("Simple attachment").length(17)
                             .contentType("text/plain")
-                            .usageType(URI.create("https://learning.dev/examples/attachments/greeting"))
+                            .usageType(URI.create("https://example.com/attachments/greeting"))
                             .addDisplay(Locale.ENGLISH, "text attachment"))
 
                         // Add binary attachment
                         .addAttachment(a -> a.content(data).length(data.length)
                             .contentType("image/jpeg")
-                            .usageType(URI.create("https://learning.dev/examples/attachments/greeting"))
+                            .usageType(URI.create("https://example.com/attachments/greeting"))
                             .addDisplay(Locale.ENGLISH, "JPG attachment"))
                         
                     )).block();
