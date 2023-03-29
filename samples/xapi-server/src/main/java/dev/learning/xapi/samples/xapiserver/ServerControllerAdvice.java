bdad6af5079a576e86af62a9dd987d9f70239729
Thomas Turrell-Croft <tom@berrycloud.co.uk>
2023-03-29 11:25:22 +0100

    Add statement repository to xAPI Server

---
+++ samples/xapi-server/src/main/java/dev/learning/xapi/samples/xapiserver/ServerControllerAdvice.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016-2023 Berry Cloud Ltd. All rights reserved.
+ */
+
+package dev.learning.xapi.samples.xapiserver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Handles exceptions in third party dependencies.
+ */
+@RestControllerAdvice
+public class ServerControllerAdvice extends ResponseEntityExceptionHandler {
+
+  /**
+   * Handles bean validation (JSR 380) exceptions and transforms them into errors that confirm to
+   * RFC 7807.
+   */
+  @ResponseBody
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ErrorResponse handleControllerException(HttpServletRequest request, Throwable e) {
+
+    // RFC 7807 error response.
+    return ErrorResponse.builder(e, HttpStatus.BAD_REQUEST, e.getMessage())
+
+        .build();
+  }
+
+}
